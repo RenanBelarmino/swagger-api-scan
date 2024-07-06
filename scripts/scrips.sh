4e2bc35bfd201e726f7733313bc3a87c00ddfda0
Renan.Belarmino <69680055+RenanBelarmino@users.noreply.github.com>
2024-07-06 11:08:10 -0300

    Update scrips.sh

--- scripts/scrips.sh
+++ scripts/scrips.sh
@@ -19,7 +19,7 @@ sudo npm install dotenv -y
 sudo chmod -R 777 node_modules
 sudo npm install dockerode
 sudo npm install jsonwebtoken bcryptjs express express-jwt
-
+sudo npm install mongoose
 
 sudo npm install -g pm2 -y
 
@@ -30,11 +30,5 @@ sudo pm2 save
 
 sudo snap install docker
 
-sudo docker run -u zap -p 8080:8080 -i zaproxy/zap-stable zap.sh -daemon -host 0.0.0.0 -port 8080 -config api.addrs.addr.name=.* -config api.addrs.addr.regex=true -config api.key=services_
-
-docker run -v "${PWD}:/zap/wrk/:rw" -t zaproxy/zap-stable zap-full-scan.py -t https://www.example.com -g gen.conf -J report_json
-docker exec dast_scan zap-full-scan.py -t https://www.example.com -g gen.conf -J report_json
-
-sudo docker start $(docker ps -aq)
-
+sudo docker compose up --build
 
